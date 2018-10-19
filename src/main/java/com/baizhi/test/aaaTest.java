ed1a45f3bb26d26db5921775a1a8243313ff1f45
root <zhaoqianglazy@163.com>
2018-10-19 17:08:00 +0800

    Second

---
+++ src/main/java/com/baizhi/test/aaaTest.java
@@ -0,0 +1,10 @@
+package com.baizhi.test;
+
+/**
+ * Created by hello world on 2018/10/19.
+ */
+public class aaaTest {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaa");
+    }
+}
